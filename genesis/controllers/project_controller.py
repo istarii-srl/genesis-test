902835e2150c93b232e21998a3c78957a3f5a0d5
treniere <tim.reniere@gmail.com>
2022-07-27 17:33:57 +0200

    preoject inactivity

---
+++ genesis/controllers/project_controller.py
@@ -0,0 +1,12 @@
+import json
+import logging
+from odoo import http
+from odoo.http import request, Response
+
+_logger = logging.getLogger(__name__)
+
+class ProjectController(http.Controller):
+
+    @http.route("/projects/get_projects_for/<int:employee_id>", type="http", auth="public", csrf=False, cors="*")
+    def get_active_project_for_user(self):
+        return ""
