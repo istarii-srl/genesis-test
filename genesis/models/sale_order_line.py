f654bd5c7f5f3461955bbda3554c859846cc56c7
treniere <tim.reniere@gmail.com>
2022-07-28 13:52:03 +0200

    fix

--- genesis/models/sale_order_line.py
+++ genesis/models/sale_order_line.py
@@ -1,6 +1,8 @@
+import logging
 from odoo import fields, api, models, _
 from odoo.exceptions import UserError
 
+_logger = logging.getLogger(__name__)
 class SaleOrderLine(models.Model):
     _name = "sale.order.line"
     _inherit = "sale.order.line"
@@ -16,6 +18,7 @@ class SaleOrderLine(models.Model):
                 for line in lines:
                     employees_for_line = { employee_id: 1 for employee_id in line.employee_ids.ids }
                     employees = { employee_id: employees.get(employee_id, 0) + employees_for_line[employee_id] for employee_id in employees_for_line.keys() }
+            _logger.info(employees)
             if (sum(employees.values()) > len(employees)):
                 raise UserError(_("Il ne peut pas y avoir un même employé sur plusieurs ligne de vente."))
 
