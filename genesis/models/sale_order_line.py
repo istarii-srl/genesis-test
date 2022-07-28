5b2a7c226341c4c725223032dcd94253a92a48ff
treniere <tim.reniere@gmail.com>
2022-07-28 12:15:06 +0200

    constraints on line

--- genesis/models/sale_order_line.py
+++ genesis/models/sale_order_line.py
@@ -1,7 +1,21 @@
-from odoo import fields, api, models
+from odoo import fields, api, models, _
+from odoo.exceptions import UserError
 
 class SaleOrderLine(models.Model):
     _name = "sale.order.line"
     _inherit = "sale.order.line"
 
     employee_ids = fields.Many2many('hr.employee', string="Employé(s)")
+
+    @api.constrains('employee_ids')
+    def ensure_no_two_employee(self):
+        for line in self:
+            order_id = line.order_id
+            employees = {}
+            for lines in order_id.order_line:
+                for line in lines:
+                    employees_for_line = { employee_id: 1 for employee_id in line.employee_ids.ids }
+                    employees = { employee_id: employees.get(employee_id, 0) + employees_for_line[employee_id] for employee_id in employees_for_line.keys() }
+            if (sum(employees.values) > len(employees)):
+                raise UserError(_("Il ne peut pas y avaoir un même employé sur plusieurs ligne de vente."))
+
