a8cedf8f5fa8fe12595ed4b83108b74b163bec84
treniere <tim.reniere@gmail.com>
2022-07-28 11:35:38 +0200

    fix

--- genesis/models/hr_employee.py
+++ genesis/models/hr_employee.py
@@ -9,7 +9,7 @@ class HrEmployeePrivate(models.Model):
     _inherit = "hr.employee"
 
     address_home_id = fields.Many2one(
-        'res.partner', 'Contact', help='Enter here the private address of the employee, not the one linked to your company.',
+        'res.partner', 'Contact lié', help='Enter here the private address of the employee, not the one linked to your company.',
         groups="hr.group_hr_user", tracking=True,
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
 
