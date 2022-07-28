f3b61e6e0cbeba5fde545a5ee1e1f1c1b41c90df
treniere <tim.reniere@gmail.com>
2022-07-28 10:48:45 +0200

    fix

--- genesis/models/hr_employee.py
+++ genesis/models/hr_employee.py
@@ -8,6 +8,15 @@ class HrEmployeePrivate(models.Model):
     @api.constrains('address_home_id')
     def ensure_single_contact(self):
         for employee in self:
+            employee.check_one_employee()
+
+    @api.onchange('address_home_id')
+    def on_address_change(self):
+        for employee in self:
+            employee.check_one_employee()
+
+    def _check_one_employee(self):
+        for employee in self:
             contact = self.env['res.partner'].search([('employee_ids', 'in', employee.id)], limit=1)
             if (contact and len(contact.employee_ids) > 1):
                 raise UserError(_("Le contact sélectionné est déjà lié à un autre employé. Un contact ne peut être lié qu'à un seul employé."))
