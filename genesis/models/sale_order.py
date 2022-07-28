196a413423a5069fdbcb8a0dec9c7004d405c5ae
treniere <tim.reniere@gmail.com>
2022-07-28 14:32:33 +0200

    fix

---
+++ genesis/models/sale_order.py
@@ -0,0 +1,27 @@
+import logging
+from odoo import fields, api, models, _
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    _name = 'sale.order'
+
+
+    def ensure_no_two_employee(self):
+        for order in self:
+            employees = {}
+            for line in order.order_line:
+                employees_for_line = { employee_id: 1 for employee_id in line.employee_ids.ids }
+                _logger.info("#############")
+                _logger.info(employees_for_line)
+                employees = { employee_id: employees.get(employee_id, 0) + employees_for_line[employee_id] for employee_id in employees_for_line.keys() }
+        _logger.info(employees)
+        if (sum(employees.values()) > len(employees)):
+            raise UserError(_("Il ne peut pas y avoir un même employé sur plusieurs ligne de vente."))
+
+    def write(self, vals):
+        super(SaleOrder, self).write(vals)
+        for order in self:
+            order.ensure_no_two_employee()
--- genesis/models/sale_order_line.py
+++ genesis/models/sale_order_line.py
@@ -9,23 +9,4 @@ class SaleOrderLine(models.Model):
 
     employee_ids = fields.Many2many('hr.employee', string="Employé(s)")
 
-    # @api.constrains('employee_ids')
-    def ensure_no_two_employee(self):
-        for line in self:
-            order_id = line.order_id
-            employees = {}
-            for lines in order_id.order_line:
-                for line in lines:
-                    employees_for_line = { employee_id: 1 for employee_id in line.employee_ids.ids }
-                    _logger.info("#############")
-                    _logger.info(employees_for_line)
-                    employees = { employee_id: employees.get(employee_id, 0) + employees_for_line[employee_id] for employee_id in employees_for_line.keys() }
-            _logger.info(employees)
-            if (sum(employees.values()) > len(employees)):
-                raise UserError(_("Il ne peut pas y avoir un même employé sur plusieurs ligne de vente."))
-
-    def write(self, vals):
-        super(SaleOrderLine, self).write(vals)
-        for line in self:
-            line.ensure_no_two_employee()
 
