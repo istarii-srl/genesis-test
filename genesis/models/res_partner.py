9fb7f9fa9160259b3b54f061d040a17cd1a19e73
treniere <tim.reniere@gmail.com>
2022-07-28 15:56:07 +0200

    fix

--- genesis/models/res_partner.py
+++ genesis/models/res_partner.py
@@ -30,14 +30,22 @@ class Partner(models.Model):
 
 
     def action_create_employee(self):
+        ignored = []
         for partner_id in self: 
-            data = {
-                'name': partner_id.name,
-                'address_home_id': partner_id.id,
-                'company_id': self.env.company.id,
-                'work_email': partner_id.email,
-                'mobile_phone': partner_id.mobile,
-                'work_phone': partner_id.phone,
-                'image_1920': partner_id.image_1920,
-            }
-            employee = self.env['hr.employee'].create(data)
+            if partner_id.company_type == 'person' and partner_id.employees_count == 0:
+                data = {
+                    'name': partner_id.name,
+                    'address_home_id': partner_id.id,
+                    'company_id': self.env.company.id,
+                    'work_email': partner_id.email,
+                    'mobile_phone': partner_id.mobile,
+                    'work_phone': partner_id.phone,
+                    'image_1920': partner_id.image_1920,
+                }
+                employee = self.env['hr.employee'].create(data)
+            else:
+                ignored.append(partner_id.name)
+        if len(ignored) > 0:
+            message = str(len(ignored)) + " contact(s) ignoré(s). Les contacts ignorées sont des entreprise ou ont déjà un employé lié. \n"
+            message += "Contacts ignorés : \n" + '\n'.join(ignored)
+            raise UserError(_(message))
