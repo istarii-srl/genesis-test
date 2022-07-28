06bd3d92c58b5ea811e62f676477850678d5de6e
treniere <tim.reniere@gmail.com>
2022-07-28 09:15:31 +0200

    fix

--- genesis/controllers/auth_controller.py
+++ genesis/controllers/auth_controller.py
@@ -5,6 +5,7 @@ class EmployeeAuthController(AuthController):
 
     def _prepare_user_data(self, partner_id, token):
         data = super()._prepare_user_data(partner_id, token)
-        employee_ids = partner_id.employee_ids
-        if len(employee_ids == 1):
-            data["employee_id"] = employee_ids[0].id
+        # employee_ids = partner_id.employee_ids
+        # if len(employee_ids == 1):
+        #     data["employee_id"] = employee_ids[0].id
+        return data
