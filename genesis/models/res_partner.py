ca9975bed1123dcd26460e06cbb5c1dd71272bc6
treniere <tim.reniere@gmail.com>
2022-07-28 09:02:54 +0200

    update security rule

---
+++ genesis/models/res_partner.py
@@ -0,0 +1,17 @@
+import logging
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+    _description = "Contacts"
+
+    def to_map(self):
+       for partner in self:
+            return {
+                "id": partner.id,
+                "name": partner.name,
+                "email": partner.email if partner.email else "",
+            }
