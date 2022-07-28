00046e5163e7ce3c324fd5034b8173d1877f6b6e
treniere <tim.reniere@gmail.com>
2022-07-28 09:39:45 +0200

    change employee fields rule and apply1-1  constr

--- genesis/models/res_partner.py
+++ genesis/models/res_partner.py
@@ -1,5 +1,6 @@
 import logging
-from odoo import models
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -7,6 +8,17 @@ _logger = logging.getLogger(__name__)
 class Partner(models.Model):
     _inherit = "res.partner"
     _description = "Contacts"
+
+    employee_ids = fields.One2many(
+        'hr.employee', 'address_home_id', string='Employees',
+        help="Related employees based on their private address")
+
+
+    @api.constrains('employee_ids')
+    def ensure_single_employee(self):
+        for partner in self:
+            if len(partner.employee_ids) > 1:
+                raise UserError(_("Un employé ne peut être lié qu'à un seul contact."))
 
     def to_map(self):
        for partner in self:
