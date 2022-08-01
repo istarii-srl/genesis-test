71551e078c795c8ad97021fcd1a9a074b44522f1
treniere <tim.reniere@gmail.com>
2022-08-01 18:16:48 +0200

    fix

--- genesis/models/analytic_line.py
+++ genesis/models/analytic_line.py
@@ -10,7 +10,7 @@ class AnalyticLine(models.Model):
         data = {
             'id': self.id,
             'date': self.date.strftime('%Y-%m-%d'),
-            'duration': self.unit_amount,
+            'duration': self._get_timesheet_time_day(),
         }
         if self.holiday_id:
             data['leave'] = self.holiday_id.holiday_status_id.to_map()
