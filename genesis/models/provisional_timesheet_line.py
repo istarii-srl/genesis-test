ea3e82169f1a2eb72d90f36d0d4f4b19b6eb15b2
treniere <tim.reniere@gmail.com>
2022-08-01 16:46:51 +0200

    fix

--- genesis/models/provisional_timesheet_line.py
+++ genesis/models/provisional_timesheet_line.py
@@ -5,6 +5,6 @@ class ProvisionalTimesheetLine(models.Model):
     _name = 'genesis.provisional.line'
     _inherit = 'account.analytic.line'
 
-    tag_ids = fields.Many2many('account.analytic.tag', 'account_analytic_line_tag_rel', 'line_id', 'tag_id')
+    tag_ids = fields.Many2many('account.analytic.tag', 'account_analytic_line_tag_rel2', 'line_id', 'tag_id')
 
     original_id = fields.Many2one('account.analytic.line', string="The original line from which this provisional was created")
