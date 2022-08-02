15f71af1f7d891b11fe1927a897c67cc84981930
treniere <tim.reniere@gmail.com>
2022-08-02 11:48:46 +0200

    fix button order

--- genesis/__manifest__.py
+++ genesis/__manifest__.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.58',
+    'version': '15.0.0.59',
 
 
     # any module necessary for this one to work correctly
