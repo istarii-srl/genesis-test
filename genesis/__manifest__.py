7364de526ea8768f253c3703005523b667cf36b4
treniere <tim.reniere@gmail.com>
2022-08-03 16:30:07 +0200

    fix

--- genesis/__manifest__.py
+++ genesis/__manifest__.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.73',
+    'version': '15.0.0.74',
 
 
     # any module necessary for this one to work correctly
--- genesis/models/calendar_event.py
+++ genesis/models/calendar_event.py
@@ -1,9 +1,11 @@
-from odoo import models, api
+from odoo import models, api, fields
 
 class Meeting(models.Model):
     _name = 'calendar.event'
     _inherit = 'calendar.event'
-    
+
+    name = fields.Char('Coucou hibou', required=True)
+
     #! copy paste ovveride to add sudo because of rights problems
     @api.model_create_multi
     def create(self, vals_list):
