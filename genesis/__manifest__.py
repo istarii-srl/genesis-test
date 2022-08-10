61bee1206e3275a23bac039d04117d6eb7234df1
treniere <tim.reniere@gmail.com>
2022-08-10 09:52:31 +0200

    timeoff code in tree view

--- genesis/__manifest__.py
+++ genesis/__manifest__.py
@@ -15,7 +15,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.96',
+    'version': '15.0.0.97',
 
 
     # any module necessary for this one to work correctly
