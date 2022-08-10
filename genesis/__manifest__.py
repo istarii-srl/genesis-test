87b5eae0b2d741ec0c0a4a9007edfc292ebe67a1
treniere <tim.reniere@gmail.com>
2022-08-10 09:43:17 +0200

    fix

--- genesis/__manifest__.py
+++ genesis/__manifest__.py
@@ -15,7 +15,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.94',
+    'version': '15.0.0.95',
 
 
     # any module necessary for this one to work correctly
