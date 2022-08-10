2a2216ab23d0b48d2e3652990c46f114c14d4022
treniere <tim.reniere@gmail.com>
2022-08-10 09:28:28 +0200

    view change in timeoff

--- genesis/__manifest__.py
+++ genesis/__manifest__.py
@@ -15,7 +15,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.92',
+    'version': '15.0.0.93',
 
 
     # any module necessary for this one to work correctly
