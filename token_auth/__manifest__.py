12ad2e2800fcbe9aeb4264eb5779e49b8179a280
Xebache <deniscapece@hotmail.com>
2022-08-18 15:15:56 +0200

    action_reset_password

--- token_auth/__manifest__.py
+++ token_auth/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.11',
+    'version': '15.0.0.12',
 
 
     # any module necessary for this one to work correctly
--- token_auth/controllers/auth_controller.py
+++ token_auth/controllers/auth_controller.py
@@ -35,6 +35,7 @@ class AuthController(http.Controller):
                 user = self._prepare_user_data(partner_id, token)
 
                 _logger.info("TOKEN => " + str(AuthToken.decode(token.token)))
+                _logger.info("RES_USER => " + str(res_user))
                 _logger.info("USER => " + str(user))
                 _logger.info("SESSION ID => " + str(request.session.sid))
                 _logger.info("SESSION TOKEN => " + str(res_user._compute_session_token(request.session.sid)))
