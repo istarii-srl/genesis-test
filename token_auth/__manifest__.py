c2984a9cf5bdfaa63bec9a929f3cda074cb7460e
Xebache <deniscapece@hotmail.com>
2022-08-19 15:08:14 +0200

    fetch public leaves => to map

--- token_auth/__manifest__.py
+++ token_auth/__manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.34',
+    'version': '15.0.0.35',
 
 
     # any module necessary for this one to work correctly
