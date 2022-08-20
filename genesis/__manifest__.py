5498c58ad3d3e6d75dc94cb8910e396e652f4022
unknown <deniscapece@hotmail.com>
2022-08-20 12:42:51 +0200

    test ?

--- genesis/__manifest__.py
+++ genesis/__manifest__.py
@@ -15,7 +15,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.103',
+    'version': '15.0.0.104',
 
 
     # any module necessary for this one to work correctly
--- genesis/models/analytic_line.py
+++ genesis/models/analytic_line.py
@@ -1,23 +1,28 @@
+import logging
 from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
 class AnalyticLine(models.Model):
-    
     _name = 'account.analytic.line'
     _inherit = 'account.analytic.line'
 
     linked_provisional_id = fields.Many2one('genesis.provisional.line', string="The provisional line created from this line")
 
     def to_map(self):
-        data = {
-            'id': self.id,
-            'date': self.date.strftime('%Y-%m-%d'),
-            'duration': self._get_timesheet_time_day(),
-        }
-        if self.holiday_id:
-            data['leave'] = self.holiday_id.to_map()
-        elif self.task_id:
-            data['task'] = self.task_id.to_map()
-        else:
-            data['project'] = self.project_id.to_map()
-            
-        
-        return data
+        for line in self:
+            _logger.info(line);
+            _logger.info("holiday => " + str(line.holiday_id));
+            _logger.info("task => " + str(line.task_id))
+            _logger.info("project => " + str(line.project_id))
+            data = {
+                'id': line.id,
+                'date': line.date.strftime('%Y-%m-%d'),
+                'duration': line._get_timesheet_time_day(),
+            }
+            if line.holiday_id:
+                data['leave'] = line.holiday_id.to_map()
+            elif line.task_id:
+                data['task'] = line.task_id.to_map()
+            else:
+                data['project'] = line.project_id.to_map()
+            return data
