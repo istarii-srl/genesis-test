f801303edaf90e6dfd13cc5b2eb6a865ad7e0c5c
unknown <deniscapece@hotmail.com>
2022-08-24 01:47:52 +0200

    refactor + clean code

--- genesis/controllers/doc_controller.py
+++ genesis/controllers/doc_controller.py
@@ -14,8 +14,10 @@ class DocController(http.Controller):
         if AuthController.is_authorized(request):
             doc = json.loads(request.params["doc"])
             try:
-                self.create_new_doc(request, doc, employee_id)
-                return request.make_response(json.dumps({"data": "Success"}))
+                new_doc = self.create_new_doc(request, doc, employee_id)
+                if new_doc:
+                    return request.make_response(json.dumps({"data": "Success"}))
+                return Response("Could not upload doc", status=404)
             
             except Exception as e:
                 _logger.info(e)
@@ -24,20 +26,25 @@ class DocController(http.Controller):
         return Response("Unauthorized", status=401)
 
     def create_new_doc(self, request, doc, employee_id):
+        employee = self.get_employee_with_folder(employee_id)
+        if employee:
+            new_doc = request.env["documents.document"].sudo().create({
+                        "datas": doc["bytes"],
+                        "partner_id": employee.address_home_id.id,
+                        "folder_id": employee.folder_id.id,
+                    })
+            if new_doc:
+                attachment = request.env['ir.attachment'].sudo().browse(new_doc.attachment_id.id)
+                attachment.name = doc["name"]
+        return new_doc
+
+    def get_employee_with_folder(self, employee_id):
         employee = request.env["hr.employee"].sudo().browse(employee_id)
-        if not employee.folder_id:
+        if employee and not employee.folder_id:
             employee_folder = request.env["documents.folder"].sudo().search([("name", "=", employee.name)])
             if not employee_folder:
                 employee_folder = request.env["documents.document"].sudo().create({
                     "name": employee.name,
                 })
             employee.folder_id = employee_folder.id
-        
-        new_doc = request.env["documents.document"].sudo().create({
-                    "datas": doc["bytes"],
-                    "partner_id": employee.address_home_id.id,
-                    "folder_id": employee.folder_id.id,
-                })
-
-        attachment = request.env['ir.attachment'].sudo().browse(new_doc.attachment_id.id)
-        attachment.name = doc["name"]
+        return employee
