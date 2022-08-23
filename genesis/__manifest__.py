309c53d3ae5e4c4748b7439038f005afd7505a83
Xebache <deniscapece@hotmail.com>
2022-08-23 15:14:10 +0200

    doc name

--- genesis/__manifest__.py
+++ genesis/__manifest__.py
@@ -15,7 +15,7 @@
     # for the full list
     'category': 'ERP',
     'license': 'OPL-1',
-    'version': '15.0.0.116',
+    'version': '15.0.0.117',
 
 
 
--- genesis/controllers/doc_controller.py
+++ genesis/controllers/doc_controller.py
@@ -13,14 +13,13 @@ class DocController(http.Controller):
         _logger.info("CONTROLLER DOC => upload")
         if AuthController.is_authorized(request):
             doc = json.loads(request.params["doc"])
-            # try:
-            document = self.create_new_doc(request, doc)
-            _logger.info(document.id)
-            return request.make_response("Success")
+            try:
+                self.create_new_doc(request, doc)
+                return request.make_response("Success")
             
-            # except Exception as e:
-            #     _logger.info(e)
-            #     return Response("Could not upload doc", status=404)
+            except Exception as e:
+                _logger.info(e)
+                return Response("Could not upload doc", status=404)
 
         return Response("Unauthorized", status=401)
 
@@ -39,11 +38,10 @@ class DocController(http.Controller):
             })
         
         doc_id = request.env["documents.document"].sudo().create({
-                    # "display_name": doc["name"],
                     "datas": doc["bytes"],
                     "partner_id": doc["user"]["id"],
                     "folder_id": workspace.id,
                 })
 
-        att_id = request.env['ir.attachment'].sudo().browse(doc_id.attachment_id.id)
-        att_id.name = doc["name"]
+        attachment_id = request.env['ir.attachment'].sudo().browse(doc_id.attachment_id.id)
+        attachment_id.name = doc["name"]
