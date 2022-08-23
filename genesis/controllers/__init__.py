4f9c3ad46b917f4b2f29e71e7745e7166dfdcc81
Xebache <deniscapece@hotmail.com>
2022-08-23 11:33:54 +0200

    doc_controller

--- genesis/controllers/__init__.py
+++ genesis/controllers/__init__.py
@@ -1,3 +1,4 @@
 from . import project_controller
 from . import timesheet_controller
 from . import leave_controller
+from . import doc_controller
---
+++ genesis/controllers/doc_controller.py
@@ -0,0 +1,25 @@
+import json
+import logging
+from odoo import http
+from odoo.http import request, Response
+from ...token_auth.controllers.auth_controller import AuthController
+
+_logger = logging.getLogger(__name__)
+
+class DocController(http.Controller):
+
+    @http.route("/doc/upload", type="http", auth="public", csrf=False, cors="*")
+    def upload_doc(self):
+        _logger.info("CONTROLLER DOC => upload")
+        if AuthController.is_authorized(request):
+            doc = json.loads(request.params["doc"])
+            try:
+                document = self.create_new_doc(request, doc)
+                _logger.info(document.id)
+                return request.make_response("Success")
+            
+            except Exception as e:
+                _logger.info(e)
+                return Response("Could not upload doc", status=404)
+
+        return Response("Unauthorized", status=401)
